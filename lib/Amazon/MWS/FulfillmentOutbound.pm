c6d6a36c399a4746edf80e3c2f4a59db30c4ffd2
RSDVD <phil.smith@rsdvd.com>
2012-02-01 16:18:33 +0000

    Added GetServiceStatus method

--- lib/Amazon/MWS/FulfillmentOutbound.pm
+++ lib/Amazon/MWS/FulfillmentOutbound.pm
@@ -4,4 +4,15 @@ use Amazon::MWS::Routines qw(:all);
 
 my $fulfillment_service = '/FulfillmentOutboundShipment/2010-10-01/';
 
+define_api_method GetServiceStatus =>
+    version => '2010-10-01',
+    raw_body => 0,
+    service => "$fulfillment_service",
+    module_name => 'Amazon::MWS::FulfillmentOutbound',
+    parameters => {},
+    respond => sub {
+        my $root = shift;
+        return $root->{Status};
+   };
+
 1;
