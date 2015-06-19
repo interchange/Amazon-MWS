04922989821ad934e8b06c68bcfc27a266b758f6
Marco Pessotto <melmothx@gmail.com>
2015-06-19 09:59:46 +0200

    Bumped up uploader version

--- lib/Amazon/MWS/Uploader.pm
+++ lib/Amazon/MWS/Uploader.pm
@@ -21,7 +21,7 @@ use Moo;
 use MooX::Types::MooseLike::Base qw(:all);
 use namespace::clean;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use constant {
     AMW_ORDER_WILDCARD_ERROR => 999999,
