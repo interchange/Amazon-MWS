557637419b3d95ef2fe676d17b1d403d83821cc8
Marco Pessotto <melmothx@gmail.com>
2020-01-28 08:49:21 +0100

    Use open with 3 arguments for logging

--- lib/Amazon/MWS/Routines.pm
+++ lib/Amazon/MWS/Routines.pm
@@ -143,8 +143,8 @@ sub define_api_method {
 
 
 	if ($self->{debug}) {
-                open LOG, ">>$self->{logfile}";
-		print LOG Dumper($response);
+                open (my $fh, '>>', $self->{logfile}) or warn("Cannot open $self->{logfile}");
+		print $fh Dumper($response);
             }
 
         my $xs = XML::Simple->new( KeepRoot => 1 );
@@ -279,11 +279,11 @@ sub new {
     die 'No marketplace id' unless $opts{marketplace_id};
 
     if ($opts{debug}) {
-       open LOG, ">$opts{logfile}" or die "Cannot open logfile.";
-       print LOG DateTime->now();
-       print LOG "\nNew instance created. \n";
-       print LOG Dumper(\%opts);
-       close LOG; 
+       open (my $fh, '>', $opts{logfile}) or die "Cannot open logfile $opts{logfile}";
+       print $fh DateTime->now();
+       print $fh "\nNew instance created. \n";
+       print $fh Dumper(\%opts);
+       close $fh;
     }
 
  # https://github.com/interchange/Amazon-MWS/issues/9
