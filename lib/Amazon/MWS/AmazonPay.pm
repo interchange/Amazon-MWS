67bf2220d8c3cb11e90b99c6e5dda6aca7aa706a
Marco Pessotto <melmothx@gmail.com>
2020-01-28 08:48:22 +0100

    Add some basic AmazonPay api calls

---
+++ lib/Amazon/MWS/AmazonPay.pm
@@ -0,0 +1,41 @@
+package Amazon::MWS::AmazonPay;
+
+use Amazon::MWS::Routines qw(:all);
+
+my $version = '2013-01-01';
+
+define_api_method (GetOrderReferenceDetails =>
+                   version => $version,
+                   parameters => {
+                                  AmazonOrderReferenceId => {
+                                                             required => 1,
+                                                             type => 'string',
+                                                            },
+                                  AccessToken => {
+                                                  required => 0,
+                                                  type => 'string',
+                                                 },
+                                 },
+                   respond => sub { return shift->{OrderReferenceDetails} },
+                  );
+
+define_api_method (SetOrderAttributes =>
+                   version => $version,
+                   raw_body => 1,
+                   parameters => {
+                                  AmazonOrderReferenceId => {
+                                                             required => 1,
+                                                             type => 'string',
+                                                            },
+                                  'OrderAttributes.OrderTotal.Amount' => { type => 'string', required => 0 },
+                                  'OrderAttributes.OrderTotal.CurrencyCode' => { type => 'string', required => 0 },
+                                  'OrderAttributes.PlatformId' => { type => 'string', required => 0 },
+                                  'OrderAttributes.SellerNote' => { type => 'string', required => 0 },
+                                  'OrderAttributes.SellerOrderAttributes.SellerOrderId' => { type => 'string', required => 0 },
+                                  'OrderAttributes.SellerOrderAttributes.StoreName' => { type => 'string', required => 0 },
+                                  'OrderAttributes.PaymentServiceProviderAttributes.PaymentServiceProviderId' => { type => 'string', required => 0 },
+                                  'OrderAttributes.PaymentServiceProviderAttributes.PaymentServiceProviderOrderId' => { type => 'string', required => 0 },
+                                 },
+                   # respond => sub { return shift->{OrderReferenceDetails} },
+                  );
+  
